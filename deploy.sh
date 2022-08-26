bca41844100bb1151f3c25d3074d692900ca822f
Emily Blanchette <emilylunella@gmail.com>
2022-08-26 10:33:41 -0500

    Build for deploy

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-ng build --prod --output-path docs --base-href /east/
+ng build --prod --output-path docs --base-href /$1/
 
 cp docs/index.html docs/404.html
 
 git add .
 
-git commit -m "$1"
+git commit -m "Build for deploy"
 
 git push
