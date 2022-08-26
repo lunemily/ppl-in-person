968ac0fe27c3ecc6f3e237bca18d4bd3e6b0111d
Emily Blanchette <emilylunella@gmail.com>
2022-08-26 09:34:04 -0500

    leader-challenger header

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -165,71 +165,9 @@ export class CameraComponent implements OnInit {
     }
 
     ngOnInit() {
-        // this.qrScannerComponent.videoElement.setAttribute('playsinline', 'true');
     }
 
     ngAfterViewInit() {
-        // this.qrScannerComponent.getMediaDevices().then(devices => {
-        //     console.log(devices);
-        //     const videoDevices: MediaDeviceInfo[] = [];
-        //     for (const device of devices) {
-        //         if (device.kind.toString() === 'videoinput') {
-        //             videoDevices.push(device);
-        //         }
-        //     }
-        //     if (videoDevices.length > 0){
-        //         let choosenDev;
-        //         for (const dev of videoDevices){
-        //             if (dev.label.includes('back') || dev.label.includes('rear')){ // Select rear-facing camera
-        //                 choosenDev = dev;
-        //                 break;
-        //             }
-        //         }
-        //         if (choosenDev) {
-        //             this.qrScannerComponent.chooseCamera.next(choosenDev);
-        //         } else {
-        //             this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
-        //         }
-        //     }
-        // });
-
-        // this.qrScannerComponent.capturedQr.subscribe(result => {
-        //     // const devURL = 'https://localhost:4200';
-        //     // const prodURL = 'https://paxpokemonleague.net/west/';
-        //     const challengerEnqueueRegex = /^http(s|):\/\/(localhost:4200|paxpokemonleague\.net\/qr)\/\?(challenger)=([a-zA-Z0-9]+){0,16}$/g;
-        //     const leaderEnqueueRegex = /^http(s|):\/\/(localhost:4200|paxpokemonleague\.net\/qr)\/\?(leader)=([a-zA-Z0-9]+){0,16}$/g;
-        //     const loginRegex = /^http(s|):\/\/(localhost:4200|paxpokemonleague.net\/qr)\/\?(leader|challenger)=([a-zA-Z0-9]+){0,16}$/g;
-        //     if (this.leaderId) {
-        //         // If a leader is scanning a challenger, the QR code will have ?challenger=
-        //         if (result.match(challengerEnqueueRegex)) {
-        //             let challengerId = result.replace("http://localhost:4200/?challenger=", "")
-        //                     .replace("http://paxpokemonleague.net/west/?challenger=", "")
-        //                     .replace("https://localhost:4200/?challenger=", "")
-        //                     .replace("https://paxpokemonleague.net/west/?challenger=", "")
-        //             this.leaderService.enqueueChallenger(this.leaderId, challengerId);
-        //         } else {
-        //             window.location.reload();
-        //         }
-        //     }
-        //     else if (this.challengerId) {
-        //         // If a challenger is scanning a leader, the QR code will have ?leader=
-        //         if (result.match(leaderEnqueueRegex)) {
-        //             let leaderId = result.replace("http://localhost:4200/?leader=", "")
-        //                     .replace("http://paxpokemonleague.net/west/?leader=", "")
-        //                     .replace("https://localhost:4200/?leader=", "")
-        //                     .replace("https://paxpokemonleague.net/west/?leader=", "")
-        //             this.leaderService.enqueueChallenger(leaderId, this.challengerId);
-        //         } else {
-        //             window.location.reload();
-        //         }
-        //     } else {
-        //         if (result.match(loginRegex)) {
-        //             window.location.replace(result);
-        //         } else {
-        //             window.location.reload();
-        //         }
-        //     }
-        // });
     }
 }
 
