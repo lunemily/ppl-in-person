5bce0c8aee789f0abeb60d2bec412c6223697352
Emily Blanchette <emilylunella@gmail.com>
2022-09-05 22:52:32 -0500

    badge on qr code

--- src/app/leader-console/leader-console.component.ts
+++ src/app/leader-console/leader-console.component.ts
@@ -12,12 +12,14 @@ export interface DialogData {
 @Component({
   selector: 'app-leader-console',
   templateUrl: './leader-console.component.html',
-  styleUrls: ['./leader-console.component.scss']
+  styleUrls: ['./leader-console.component.scss'],
 })
 export class LeaderConsoleComponent implements OnInit {
+  badgeImageUrl = 'assets/images/pokeball.png';
+  leaderImageUrl = 'assets/images/pokeball.png';
   myAngularxQrCode: string;
   battleCode: string;
-  showCamera: boolean
+  showCamera: boolean;
   options: UntypedFormGroup;
   hideRequiredControl = new UntypedFormControl(false);
   floatLabelControl = new UntypedFormControl('auto');
@@ -27,31 +29,28 @@ export class LeaderConsoleComponent implements OnInit {
 
   @Input() leader: Leader;
 
-  constructor(public dialog: MatDialog,
-    private leaderService: LeaderService,) { }
+  constructor(public dialog: MatDialog, private leaderService: LeaderService) {}
 
   ngOnInit(): void {
-    this.showCamera = false
+    this.showCamera = false;
     this.myAngularxQrCode = `https://paxpokemonleague.net/qr/?leader=${this.leader.leaderId}`;
     this.battleCode = this.generateCode();
   }
 
   enqueueQR(): void {
     if (this.showCamera === false) {
-      this.showCamera = true
+      this.showCamera = true;
     } else {
-      this.showCamera = false
+      this.showCamera = false;
     }
   }
 
   generateCode(): string {
-    let code = "";
-    let possible = "0123456789";
-  
-    for (let i = 0; i < 8; i++)
-      code += possible.charAt(Math.floor(Math.random() * possible.length));
-  
+    let code = '';
+    let possible = '0123456789';
+
+    for (let i = 0; i < 8; i++) code += possible.charAt(Math.floor(Math.random() * possible.length));
+
     return code.match(/.{1,4}/g).join(' ');
   }
-
 }
