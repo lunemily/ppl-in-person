e31ab2ed604aaf7dd6ac5d5f85991a6e89e41fa7
Emily Blanchette <emilylunella@gmail.com>
2022-09-11 23:11:13 -0500

    routing solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutLeadersComponent } from './about-leaders/about-leaders.component';
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: 'about', component: AboutLeadersComponent },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
