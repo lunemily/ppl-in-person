4c487680f2e949f8d9698efcd1c0260f66e6ac66
Emily Blanchette <emilylunella@gmail.com>
2024-08-05 21:46:03 -0500

    Build for deploy west

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@ Set-Variable -Name "ppl" -Value $args[0]
 
 Set-Variable -Name "env" -Value $args[1]
 
-ng build --output-path docs\$ppl\$env --base-href /$ppl --configuration=$ppl$env
+ng build --output-path docs\$ppl\$env --base-href /$ppl/ --configuration=$ppl$env
 
 Copy-Item ".\docs\$ppl\$env\index.html" ".\docs\$ppl\$env\404.html"
 
