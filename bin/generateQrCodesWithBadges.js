67995ba8a4eda59e331b51edfb2a04e4435e5e34
Emily Blanchette <emilylunella@gmail.com>
2025-08-26 19:22:32 -0400

    Build artifact for west Stg

--- bin/generateQrCodesWithBadges.js
+++ bin/generateQrCodesWithBadges.js
@@ -1,62 +1,72 @@
-import QRCode from 'qrcode'
-import { createCanvas, loadImage } from 'canvas'
-import fs from 'fs'
+import QRCode from 'qrcode';
+import { createCanvas, loadImage } from 'canvas';
+import fs from 'fs';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const prod = false;
+const port = prod ? 26438 : 26441;
+const paxEvent = 'west';
+
+function cleanup() {
+  try {
+    fs.rmdirSync('bin/qr');
+  } catch (error) {}
+  fs.mkdirSync('bin/qr');
+}
 
 async function create(dataForQRcode, center_image, width, cwidth) {
   const canvas = createCanvas(width, width);
-  QRCode.toCanvas(
-    canvas,
-    dataForQRcode,
-    {
-      errorCorrectionLevel: "H",
-      margin: 1,
-      color: {
-        dark: "#000000",
-        light: "#ffffff",
-      },
-    }
-  );
+  QRCode.toCanvas(canvas, dataForQRcode, {
+    errorCorrectionLevel: 'H',
+    margin: 1,
+    color: {
+      dark: '#000000',
+      light: '#ffffff',
+    },
+  });
 
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext('2d');
   const img = await loadImage(center_image);
   const center = (width - cwidth) / 2;
   ctx.drawImage(img, center, center, cwidth, cwidth);
-  return canvas.toDataURL("image/png");
+  return canvas.toDataURL('image/png');
 }
 
 async function main(leader) {
-  const qrCode = await create(
-    `https://paxpokemonleague.net/east/?qr=true&opponentId=${leader.id}&opponentType=leader`,
-    `https://toastserv.com:26438/static/badges/${leader.id}.png`,
-    225,
-    100
-  );
-  // Remove header if present
-  const base64Data = qrCode.replace(/^data:image\/\w+;base64,/, '');
+  try {
+    const qrCode = await create(
+      `https://paxpokemonleague.net/${paxEvent}/?qr=true&opponentId=${leader.id}&opponentType=leader`,
+      `https://toastserv.com:${port}/static/badges/${leader.id}.png`,
+      225,
+      100,
+    );
+    // Remove header if present
+    const base64Data = qrCode.replace(/^data:image\/\w+;base64,/, '');
 
-  // Create a buffer from the base64 string
-  const buffer = Buffer.from(base64Data, 'base64');
+    // Create a buffer from the base64 string
+    const buffer = Buffer.from(base64Data, 'base64');
 
-  fs.writeFileSync(`bin/qr/${leader.name.replaceAll("\"", "")}.png`, buffer)
+    fs.writeFileSync(`bin/qr/${leader.name.replaceAll('"', '')}.png`, buffer);
+  } catch (err) {
+    console.error(`Failed on ${leader.name}: ${err}`);
+  }
 }
 
 async function getLeaderData() {
-  const rawLeaderData = await (await fetch('https://toastserv.com:26438/api/v2/allleaderdata')).json()
+  const rawLeaderData = await (await fetch(`https://toastserv.com:${port}/api/v2/allleaderdata`)).json();
   const leaderData = Object.keys(rawLeaderData).map((leadrId) => {
     return {
       id: leadrId,
-      name: rawLeaderData[leadrId]["name"]
-    }
-  })
+      name: rawLeaderData[leadrId]['name'],
+    };
+  });
   return leaderData;
 }
 
-const leader = {
-  id: 'ddf034dfc811',
-  name: 'Titania, Fae Queen of Il Mheg'
-}
+cleanup();
 
-const leaderData = await getLeaderData()
+const leaderData = await getLeaderData();
+console.info(JSON.stringify(leaderData));
 leaderData.map((leader) => {
-  main(leader)
-})
+  main(leader);
+});
