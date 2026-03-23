1ecebcebf3cdb80d6f046225a3cd0dba85377e97
Emily Blanchette <emilylunella@gmail.com>
2026-03-22 20:54:05 -0400

    Build artifact for east Stg

--- bin/generateQrCodesWithBadges.js
+++ bin/generateQrCodesWithBadges.js
@@ -6,11 +6,45 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const prod = false;
 const port = prod ? 26438 : 26441;
 const prodPort = 26438;
-const paxEvent = 'unplugged';
+const paxEvent = 'east';
 
 function cleanup() {
   fs.rmdirSync('bin/qr', { recursive: true, force: true });
   fs.mkdirSync('bin/qr');
+}
+
+async function getLeaderData() {
+  const rawLeaderData = await (await fetch(`https://toastserv.com:${port}/api/v2/allleaderdata`)).json();
+  const leaderData = Object.keys(rawLeaderData).map((leaderId) => {
+    return {
+      id: leaderId,
+      name: rawLeaderData[leaderId]['name'],
+    };
+  });
+
+  // add bingler
+  if (['west'].includes(paxEvent)) {
+    leaderData.push({ id: 'be24de2c8b94', name: 'Lord Bingler, King of the Bingo Hall' });
+  }
+  // add followingler
+  // leaderData.push({ id: 'cddaba15d491', name: 'Lord Fingler, the Socialite' });
+
+  // add artingler
+  if (['unplugged'].includes(paxEvent)) {
+    leaderData.push({ id: 'f00c087d1a2c', name: 'Lord Fingler, the Artiste' });
+  }
+
+  // add Guesler
+  if (['unplugged'].includes(paxEvent)) {
+    leaderData.push({ id: 'be24de2c8b94', name: 'Lord Guesler, the Detective' });
+  }
+
+  // add other fingler (TBD)
+  if (['east'].includes(paxEvent)) {
+    leaderData.push({ id: 'a5ef568da9c6', name: 'cookieb' });
+  }
+
+  return leaderData;
 }
 
 async function create(dataForQRcode, center_image, width, cwidth) {
@@ -31,7 +65,7 @@ async function create(dataForQRcode, center_image, width, cwidth) {
   return canvas.toDataURL('image/png');
 }
 
-async function main(leader) {
+async function createAndSaveQrImage(leader) {
   try {
     const qrCode = await create(
       `https://paxpokemonleague.net/${paxEvent}/?qr=true&opponentId=${leader.id}&opponentType=leader`,
@@ -45,40 +79,21 @@ async function main(leader) {
     // Create a buffer from the base64 string
     const buffer = Buffer.from(base64Data, 'base64');
 
-    fs.writeFileSync(`bin/qr/${leader.name.replaceAll('"', '')}.png`, buffer);
+    fs.writeFileSync(`bin/qr/${leader.name.replaceAll('"|<|>', '')}.png`, buffer);
   } catch (err) {
     console.error(`Failed on ${leader.name}: ${err}`);
   }
 }
 
-async function getLeaderData() {
-  const rawLeaderData = await (await fetch(`https://toastserv.com:${port}/api/v2/allleaderdata`)).json();
-  const leaderData = Object.keys(rawLeaderData).map((leadrId) => {
-    return {
-      id: leadrId,
-      name: rawLeaderData[leadrId]['name'],
-    };
-  });
-  return leaderData;
-}
-
 cleanup();
+
+console.info(`PAXEvent: ${paxEvent}`);
+console.info(`Env: ${prod ? 'prod' : 'stage'}`);
 
 const leaderData = await getLeaderData();
 
-// add bingler
-// leaderData.push({ id: 'be24de2c8b94', name: 'Lord Bingler, King of the Bingo Hall' });
-
-// add followingler
-// leaderData.push({ id: 'cddaba15d491', name: 'Lord Fingler, the Socialite' });
-
-// add artingler
-leaderData.push({ id: 'f00c087d1a2c', name: 'Lord Fingler, the Artiste' });
-
-// add Guesler
-leaderData.push({ id: 'be24de2c8b94', name: 'Lord Guesler, the Detective' });
-
 console.info(JSON.stringify(leaderData));
+
 leaderData.map((leader) => {
-  main(leader);
+  createAndSaveQrImage(leader);
 });
